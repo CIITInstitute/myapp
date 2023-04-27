19d333e3af13971b0ff90d1098f0b15fc5697425
shwetav3 <118896883+shwetav3@users.noreply.github.com>
2023-04-27 19:52:52 +0530

    Update SampleComponent.js

--- src/SampleComponent.js
+++ src/SampleComponent.js
@@ -4,6 +4,14 @@ export const Sample =()=>{
   return(
 <div>
     <h2>Welcome</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Roll No</th>
+          <th>Student Name</th>
+        </tr>
+      </thead>
+    </table>
 </div>
   )
 
